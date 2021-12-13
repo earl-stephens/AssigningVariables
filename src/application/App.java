770d375433c17edf23d8de2a17cb4bda949930cd
earl-stephens <stephensearl@hotmail.com>
2021-12-13 12:37:07 -0700

    Added code for assigning variables and printing them

--- src/application/App.java
+++ src/application/App.java
@@ -2,9 +2,32 @@ package application;
 
 public class App {
 
+	/*
+	 * Assigning values to variables.
+	 * This is a multiline comment.
+	 */
+	
+	// This is a single line comment.
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int dogs = 8;
+		int cats = 3;
+		
+		System.out.println("1. Dogs: " + dogs);
+		System.out.println("1. Cats: " + cats);
 
+		dogs = cats;
+		cats = 10;
+		
+		/* Take the value assigned to cats
+		 * and assign it to dogs.
+		 * = is an assignment operator in this case,
+		 * not an equality operator.
+		 */
+		
+		System.out.println("2. Dogs: " + dogs);
+		System.out.println("2. Cats: " + cats);
 	}
 
 }
